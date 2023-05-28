25b6b1a8f163c9f6fa96913c2c78ea9091ba48d2
Bianca Sofia <sofiabrasil44@gmail.com>
2023-05-28 18:52:17 -0300

    adicionando fase 1 e 2
    Co-Authored-By: Daniela Soares de Oliveira <56639683+daniso123@users.noreply.github.com>
    Co-Authored-By: vitorekr <56610229+vitorekr@users.noreply.github.com>

--- src/main.pl
+++ src/main.pl
@@ -40,19 +40,57 @@ suspeito_culpado(Suspeito).
 % Second Version
 
 :- dynamic suspeito/1.
+:- dynamic encontrou_pista/1.
 
-suspeito(jogador).
-suspeito(james).
-suspeito(jessie).
-suspeito(giovanni).
+
+suspeito(BenjaminBlackwood).
+suspeito(VictoriaSinclair).
+suspeito(EthanDonova).
+suspeito(OliviaMorgan).
+suspeito(SebastianRusso).
+suspeito(IsabellaSantiago).
+suspeito(JacobGallagher).
+suspeito(SophiaChen).
+suspeito(GabrielRamirez).
+
+cenario(lobby_hotel).
+cenario(praia).
+cenario(celas).
+cenario(escritorio).
+cenario(cozinha).
+cenario(quarto).
+cenario(montanha).
+
+motivacao(benjamin_blackwood, vender).
+motivacao(victoria_sinclair, crente_pokemon).
+motivacao(ethan_donovan, equipe_rocket).
+motivacao(olivia_morgan, tava_afim_de_ver_o_mar).
+motivacao(sebastian_russo, pesquisador_pokemon).
+motivacao(isabella_santiago, mae_doente).
+motivacao(jacob_gallagher, treinador_linhagem).
+motivacao(sophia_chen, poder).
+motivacao(gabriel_ramirez, vinganca).
 
 % Definição das fases
+% Fase inicial
 fase(1) :-
-    nl, write('Você está em uma floresta escura e encontra um Pokémon ferido.'),
-    nl, write('O que você faz?'),
-    nl, write('1. Levar o Pokémon para o centro Pokémon mais próximo.'),
-    nl, write('2. Ignorar o Pokémon e continuar sua jornada.'),
-    nl, write('3. Capturar o Pokémon para si.'), nl,
+    nl, write('Bem-vindo(a) ao jogo de mistério "Pokémon Crime Investigation"!'), nl,
+    write('Você se encontra no luxuoso lobby de um hotel quando de repente um misterioso pokémon sai voando do topo da montanha emitindo um estrondoso som que ecoou por toda a ilha, e em um ato de fúria o pokémon dispara um poderoso ataque explosivo contra a ilha'), nl,
+    write('Após desaparecer, um senhor idoso grita "LUGIA!". Será possível? Apesar do nome da ilha, acreditava-se que Lugia não estava mais no local...'), nl,
+    write('Uma misteriosa voz começa a ecoar em sua cabeça, o que e quem seria?'), nl,
+    write('De repente um pokémon aparece e se apresenta como Celebi e conta que o ovo do poderoso Lugia foi roubado e agora está em fúria, que só poderá ser cessada quando o ovo for recuperado...'), nl,
+    write('Sua missão é desvendar esse intrigante crime, identificar o culpado e devolver o ovo para Lugia.'), nl,
+    write('Cada suspeito possui uma ligação com um Pokémon específico, e suas ações e relações podem revelar pistas importantes para chegar à verdade.'), nl,
+    write('Explore os diversos locais do hotel, como as celas, o escritório, a cozinha, o quarto e até mesmo a Montanha próxima.'), nl,
+    write('Lembre-se de que nem todas as evidências foram encontradas e nem todos os suspeitos são culpados.'), nl,
+    write('Faça perguntas estratégicas, analise cuidadosamente as informações disponíveis e use sua sagacidade para chegar à conclusão correta.'), nl,
+    write('Você está pronto(a) para enfrentar o desafio de "Pokémon Crime Investigation" e solucionar esse enigma intrigante?'), nl,
+    write('A verdade espera por você no coração deste hotel. Boa sorte, detetive!'), nl,
+    nl, write('Você está no lobby do hotel e um ovo de Pokémon foi roubado.'), nl,
+    nl, write('O que você faz?'), nl,
+    nl, write('1. Investigar a praia.'), nl,
+    write('2. Falar com algum suspeito.'), nl,
+    write('3. Procurar pistas no hotel.'), nl,
     read(Opcao),
     (
         Opcao = 1 -> fase(2);
@@ -60,17 +98,93 @@ fase(1) :-
         Opcao = 3 -> fase(4)
     ).
 
+% Fase da praia
 fase(2) :-
-    nl, write('No centro Pokémon, o enfermeiro Joy agradece por ajudar o Pokémon.'),
-    nl, write('Você nota que o Pokémon tem uma marca estranha no corpo.'),
-    nl, write('O que você faz?'),
-    nl, write('1. Perguntar ao enfermeiro Joy sobre a marca.'),
-    nl, write('2. Ignorar a marca e sair do centro Pokémon.'), nl,
+    cenario(praia),
+    write('Você está na praia. O que você faz?'), nl,
+    write('1. Investigar os arredores.'), nl,
+    write('2. Procurar pegadas na areia.'), nl,
+    write('3. Conversar com outros hóspedes.'), nl,
     read(Opcao),
     (
-        Opcao = 1 -> fase(5);
-        Opcao = 2 -> fase(6)
+        Opcao = 1 -> acao_investigar_arredores;
+        Opcao = 2 -> acao_procurar_pegadas;
+        Opcao = 3 -> fase(3)
     ).
+
+acao_procurar_pegadas:-
+write('Você está investigando os arredores da praia. O que você encontra?'), nl,
+    write('Você encontra pegadas de algum animal ou pokémon que se arrasta'), nl,
+    write('O que você faz?'), nl,
+    write('1. Investigar melhor os arredores.'), nl,
+    write('2. Falar com suspeito.'), nl,
+    write('3. Voltar.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_investigar_arredores;
+        Opcao = 2 -> fase(3);
+        Opcao = 2 -> fase(2)
+    ).
+
+
+% Função para processar a ação de investigar os arredores na praia
+acao_investigar_arredores :-
+    write('Você está investigando os arredores da praia. O que você encontra?'), nl,
+    write('Você encontra uma mochila abandonada e uma partitura musical.'), nl,
+    write('O que você faz?'), nl,
+    write('1. Investigar a mochila.'), nl,
+    write('2. Investigar partitura musical.'), nl,
+    write('3. Voltar.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_investigar_mochila;
+        Opcao = 2 -> acao_investigar_partitura;
+        Opcao = 3 -> fase(2)
+    ).
+
+% Função para processar a ação de investigar a mochila na praia
+acao_investigar_mochila :-
+    write('Você investiga a mochila. O que você encontra?'), nl,
+    write('Você encontra uma tabela de valores de pokémon, mapa do local, recibo de transações suspeitas'), nl,
+    % Atualiza a lista de pistas encontradas
+    assert(encontrou_pista(tabela_valores_pokemon)),
+    assert(encontrou_pista(mapa_local)),
+    assert(encontrou_pista(recibo_transacoes)),
+    
+    write('Você sabe quem é o dono da mochila.'), nl,
+    write('O que você faz?'), nl,
+    write('1. Continua investigando arredores.'), nl,
+    write('2. Falar com o suspeito.'), nl,
+    write('3. Ir para a floresta.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_investigar_arredores;
+        Opcao = 2 -> fase(3); 
+        Opcao = 3 -> fase(4)
+    ).
+
+acao_investigar_partitura :-
+    write('Você investiga a partitura. O que você encontra?'), nl,
+    write('Você encontra uma partitura musical com anotações sobre o canto encantador de Primarina.'), nl,
+    % Atualiza a lista de pistas encontradas
+    assert(encontrou_pista(partitura_musical)),
+    
+    write('Você sabe quem é o dono da partitura.'), nl,
+    write('O que você faz?'), nl,
+    write('1. Continua investigando arredores.'), nl,
+    write('2. Falar com o suspeito.'), nl,
+    write('3. Ir para a floresta.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_investigar_arredores;
+        Opcao = 2 -> fase(3); 
+        Opcao = 3 -> fase(4)
+    ).
+
+
+
+
+% Template
 
 fase(3) :-
     nl, write('Enquanto continua sua jornada, você encontra um suspeito.'),
