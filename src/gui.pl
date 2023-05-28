4e9cf4493e062b9e714523869ad0093a5a5f77fc
adrian_s_lps <adriansoareslps@gmail.com>
2023-05-28 15:40:16 -0300

    Testing Image in GUI
    Co-authored-by: Daniel <DanielViniciusAlves@users.noreply.github>
    Co-authored-by: Eduardo <eduardomr@users.noreply.github>
    Co-authored-by: Klyssmann <klyssmannoliveira@users.noreply.github>

--- src/gui.pl
+++ src/gui.pl
@@ -3,9 +3,21 @@
 :- pce_begin_class(name_asker, dialog,
                    "Modal prompter for a name").
 
+variable(image, image, get, "Image"). % New variable for the image
+
 initialise(W, Label:[name]) :->
         "Initialise the window and fill it"::
         send_super(W, initialise(Label)),
+
+         % Load and display the image
+        new(Img, image('/home/adriansl/Imagens/photo_5145374158253829255_y.jpg')),
+        new(B, bitmap(Img)),
+        send(W, slot, image, B),
+        send(W, display, new(P, picture)),
+        send(P, display, B, point(0, 0)),
+        %send(W, display, new(P, picture)),
+        %send(P, display, Img, point(0, 0)),
+
         send(W, append(text_item(name))),
         send(W, append(button(ok))),
         send(W, append(button(cancel))),
