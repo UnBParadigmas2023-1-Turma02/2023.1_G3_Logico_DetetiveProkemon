1a2fe339c2e8fabb28036d190e47b88f1595d64d
joaobisi <jbisinotti@gmail.com>
2023-05-30 02:28:44 -0300

    iniciando criação da fase 4
    Co-authored-by: MarianaPRio <marianapires00@gmail.com>
    Co-authored-by: vitorekr <vitorekrlol@gmail.com>

--- src/main.pl
+++ src/main.pl
@@ -55,6 +55,7 @@ suspeito(GabrielRamirez).
 
 cenario(lobby_hotel).
 cenario(praia).
+cenario(floresta).
 cenario(celas).
 cenario(escritorio).
 cenario(cozinha).
@@ -545,6 +546,66 @@ gabrielRamirez :-
         Opcao = 2 -> gabrielRamirez
     ).  
 
+fase(4) :-
+    cenario(floresta),
+    write('Você está na floresta. O que você faz?'), nl,
+    write('1. Investigar a mata densa.'), nl,
+    write('2. Seguir a trilha.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_investigar_mata;
+        Opcao = 2 -> acao_seguir_trilha;
+    ).
+
+acao_investigar_mata :-
+    write('Você está investigando a vegetação densa da floresta. O que você encontra?'), nl,
+    % descrever pistas encontradas
+    write('O que você faz?'), nl,
+    write('1. Investigar mais a mata.'), nl,
+    write('2. Voltar.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_investigar_mata;
+        Opcao = 2 -> fase(4)
+    ).
+
+acao_seguir_trilha :-
+    write('Você decide seguir uma trilha na floresta. Para onde ele leva?'), nl,
+    write('Ela te leva até uma clareira onde tem um acampamento'), nl,
+    write('O que você faz?'), nl,
+    write('1. Investiga o local.'), nl,
+    write('2. Conversar com pessoa acampada.'), nl,
+    write('3. Voltar.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_investigar_acampamento;
+        Opcao = 2 -> acao_conversar_acampado;
+        Opcao = 3 -> fase(4)
+    ).    
+
+acao_investigar_acampamento :-
+    write('Você está investigando o acampamento. O que você encontra?'), nl,
+    % descrever pistas encontradas
+    write('O que você faz?'), nl,
+    write('1. Investigar mais o acampamento.'), nl,
+    write('2. Conversar com pessoa acampada.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_investigar_acampamento;
+        Opcao = 2 -> acao_conversar_acampado
+    ).   
+
+acao_conversar_acampado :-
+    write('Você decide com pessoa acampada. O que ela diz?'), nl,
+    write('Ela te diz como chegar até a base da montanha.'), nl,
+    write('O que você faz?'), nl,
+    write('1. Investiga mais o local.'), nl,
+    write('2. Segue para a montanha.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_investigar_acampamento;
+        Opcao = 2 -> fase(?)
+    ).         
 
 % % Template que está abaixo
 
