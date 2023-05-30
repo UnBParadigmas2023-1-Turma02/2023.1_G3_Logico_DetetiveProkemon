8166df8c9172622a832dae6d808760f1b878a272
06468709121 <dannysoaresdeoliveira@gmail.com>
2023-05-29 23:23:44 -0300

    Criando perguntas para os suspeitos Co-authored-by: Bianca Sofia <sofiabrasil44@gmail.com>

--- src/main.pl
+++ src/main.pl
@@ -354,6 +354,196 @@ benjaminBlackwood:-
         Opcao = 2 -> ethanDonovan
     ).
 
+oliviaMorgan:-
+    
+    write('Você está investigando Olivia Morgan. O que você quer perguntar?'), nl,
+    write('1. Fale sobre você, por que você está aqui? e qual seu Pokémon? '), nl,
+    write('2. O que você fez desde que chegou na ilha? .'), nl,
+    write('3. Falar com mais suspeitos.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_sobre_OliviaMorgan;
+        Opcao = 2 -> acao_paradeiro_OliviaMorgan;
+        Opcao = 2 -> fase(3)
+    ).
+    acao_sobre_OliviaMorgan:-
+      write('Resposta: Vim  a ilha com o objetivo de ver o mar, já que estava de férias. Meu pokémon é o Dragonite'), nl,
+      write('2. Fazer mais perguntas pro suspeito.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> oliviaMorgan
+    ).
+    acao_paradeiro_OliviaMorgan:-
+      write('Resposta: Eu estava na praia banhando no mar quando o fato ocorreu, estava bem longe da montanha.'), nl,
+      write('1. Voltar para o menu de suspeitos.'), nl,
+      write('2. Fazer mais perguntas pro suspeito.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> oliviaMorgan
+    ).  
+
+
+
+
+sebastianRusso:-
+    
+    write('Você está investigando Benjamin Blackwood. O que você quer perguntar?'), nl,
+    write('1. Fale sobre você, por que você está aqui? e qual seu Pokémon? '), nl,
+    write('2. O que você fez desde que chegou na ilha? .'), nl,
+    write('3. Falar com mais suspeitos.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_sobre_SebastianRusso;
+        Opcao = 2 -> acao_paradeiro_SebastianRusso;
+        Opcao = 2 -> fase(3)
+    ).
+    acao_sobre_SebastianRusso:-
+      write('Resposta: Vim para ilha com o objetivo de pesquisar sobre a evolução de Pokémon de planta. Meu Pokémon é o Serperior'), nl,
+      write('1. Voltar para o menu de suspeitos.'), nl,
+      write('2. Fazer mais perguntas pro Benjamin.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> sebastianRusso
+    ).
+      
+    acao_paradeiro_SebastianRusso:-
+      write('Resposta: Durante o horário do roubo do ovo de Pokémon, eu estava no hotel da ilha. Tinha acabado de retornar de uma longa jornada de pesquisa na floresta e decidiu descansar em seu quarto. E não sair do hotel durante todo o período em que o roubo ocorreu. você pode confirmar que estive no meu quarto pelo registro do hotel, que mostra meu check-in e a permanência em meu quarto durante o horário do roubo.'), nl,
+      write('1. Voltar para o menu de suspeitos.'), nl,
+      write('2. Fazer mais perguntas pro suspeito.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> sebastianRusso
+    ).
+
+
+isabellaSantiago:-
+    
+    write('Você está investigando Benjamin Blackwood. O que você quer perguntar?'), nl,
+    write('1. Fale sobre você, por que você está aqui e qual seu Pokémon? .'), nl,
+    write('2. O que você fez desde que chegou na ilha? .'), nl,
+    write('3. Falar com mais suspeitos.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_sobre_isabellaSantiago;
+        Opcao = 2 -> acao_paradeiro_isabellaSantiago;
+        Opcao = 2 -> fase(3)
+    ).
+    acao_sobre_isabellaSantiago:-
+      write('Resposta: Vim conhecer a ilha para encontrar uma cura para minha mãe, ouvir falar que existe uma espécie de Pokémon de cura, vim saber mais a respeito. Meu pokémon é o Gengar.'), nl,
+      write('1. Voltar para o menu de suspeitos.'), nl,
+      write('2. Fazer mais perguntas pro Benjamin.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> isabellaSantiago
+    ).
+      
+    acao_paradeiro_isabellaSantiago:-
+      write('Resposta: Eu estava na .'), nl,
+      write('1. Voltar para o menu de suspeitos.'), nl,
+      write('2. Fazer mais perguntas pro suspeito.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> isabellaSantiago
+    ).
+
+
+
+jacobGallagher :-
+    
+    write('Você está investigando Jacob Gallagher. O que você quer perguntar?'), nl,
+    write('1. Fale sobre você, por que você está aqui? e qual seu Pokémon? '), nl,
+    write('2. O que você fez desde que chegou na ilha? .'), nl,
+    write('3. Falar com mais suspeitos.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_sobre_JacobGallagher;
+        Opcao = 2 -> acao_paradeiro_JacobGallagher;
+        Opcao = 2 -> fase(3)
+    ).
+    acao_sobre_JacobGallagher:-
+      write('Resposta: Vim  a ilha com o objetivo de treinar meu pokémon, acredito que a ilha tenha uma força energética e pude encontrar meu lugar nesta ilha. Sou de uma linhagem de antigos treinadores de Luguia . Meu pokémon é o Primarina.'), nl,
+      write('2. Fazer mais perguntas pro suspeito.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> jacobGallagher
+    ).
+    acao_paradeiro_JacobGallagher:-
+      write('Resposta: Eu estava na montanha meditando, pois lá sentia uma energia.'), nl,
+      write('1. Voltar para o menu de suspeitos.'), nl,
+      write('2. Fazer mais perguntas pro suspeito.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> jacobGallagher
+    ).  
+
+sophiaChen :-
+    
+    write('Você está investigando Jacob Gallagher. O que você quer perguntar?'), nl,
+    write('1. Fale sobre você, por que você está aqui? e qual seu Pokémon? '), nl,
+    write('2. O que você fez desde que chegou na ilha? .'), nl,
+    write('3. Falar com mais suspeitos.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_sobre_SophiaChen;
+        Opcao = 2 -> acao_paradeiro_SophiaChen;
+        Opcao = 2 -> fase(3)
+    ).
+    acao_sobre_SophiaChen:-
+      write('Resposta: Sou apaixonada por criaturas lendárias e itens raros. Meu pokémon é o Weavile, ele me ajuda em minhas jornadas e aventuras, sempre prontos para enfrentar qualquer obstáculo que surja pelo caminho."'), nl,
+      write('2. Fazer mais perguntas pro suspeito.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> SophiaChen
+    ).
+    acao_paradeiro_SophiaChen:-
+      write('Resposta: Eu estava perto da montanha fazendo trilha e buscando pedras preciosas com meu Weavile. Ouvi dizer que naquele local existiam diversas pedras raras.'), nl,
+      write('1. Voltar para o menu de suspeitos.'), nl,
+      write('2. Fazer mais perguntas pro suspeito.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> SophiaChen
+    ).  
+
+
+gabrielRamirez :-
+    
+    write('Você está investigando Jacob Gallagher. O que você quer perguntar?'), nl,
+    write('1. Fale sobre você, por que você está aqui? e qual seu Pokémon? '), nl,
+    write('2. O que você fez desde que chegou na ilha? .'), nl,
+    write('3. Falar com mais suspeitos.'), nl,
+    read(Opcao),
+    (
+        Opcao = 1 -> acao_sobre_gabrielRamirez;
+        Opcao = 2 -> acao_paradeiro_gabrielRamirez;
+        Opcao = 2 -> fase(3)
+    ).
+    acao_sobre_gabrielRamirez:-
+      write('Resposta: Vim para ilha poque meu pai adorava viajar pra cá quria está mais perto dele. Meu pokémon é o Greninja, ele veio me acompanhar nessa viajem e me proteger"'), nl,
+      write('2. Fazer mais perguntas pro suspeito.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> gabrielRamirez
+    ).
+    acao_paradeiro_gabrielRamirez:-
+      write('Resposta: Eu estava na praia treinando com meu Pokémon Greninja caso você não saiba ele é da categoria ninja.'), nl,
+      write('1. Voltar para o menu de suspeitos.'), nl,
+      write('2. Fazer mais perguntas pro suspeito.'), nl,
+      read(Opcao),
+    (
+        Opcao = 1 -> fase(3);
+        Opcao = 2 -> gabrielRamirez
+    ).  
 
 
 % % Template que está abaixo
