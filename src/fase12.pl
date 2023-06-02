b98e701a55d80fd7773e2e3fc0623229811cdde3
adrian_s_lps <adriansoareslps@gmail.com>
2023-06-01 23:07:56 -0300

    Adds fase12 e fase13
    Co-authored-by: João Joao <joaobisi@users.noreply.github.com>
    Co-authored-by: Vitor <vitorekr@users.noreply.github.com>

---
+++ src/fase12.pl
@@ -0,0 +1,96 @@
+:- module(fase12, [fase12/2]).
+:- use_module('fase10').
+:- use_module('fase8').
+
+fase12(12, Parent) :-
+    free(Parent), % Destrói a janela anterior
+    ScreenText = 'Quarto do Ethan',
+    ImagePath = './images/generic.jpg',
+
+    % Configuração dos componentes de texto
+    Title = ['Você chegou ao quarto de Ethan, está bem organizado... Quem diria...''Você investiga Jessie e descobre evidências ligando-a ao crime.'],
+    Body = [
+    'O que você faz?',
+    '1. Procurar pistas.',
+    '2. Interrogar Ethan',
+    '3. Escolher culpado'
+    ],
+
+    init_fase(
+      ScreenText, ImagePath, Title, Body, Dialog, Canvas, BG, BGroup, GGroup, TGroup, SGroup
+    ),
+
+    retractall(suspeito(jessie)),
+
+    % Configuração dos botões
+    send(BGroup, append, button("Procurar Pistas", message(@prolog, acao_procurar_pistas_ethan, Dialog))),
+    send(BGroup, append, button("Examinar Quarto", message(@prolog, acao_examinar_quarto_ethan, Dialog))),
+    send(BGroup, append, button("Prosseguir", message(@prolog, fase10, 10, Dialog), Dialog))),
+
+    % Configuração do botão de lista de suspeitos
+    send(SGroup, append, button("lista de suspeitos", message(@prolog, lista_suspeitos, 2, Dialog, bitmap('images/lista_suspeitos.jpg')))),
+
+    % Posicionamento dos componentes
+    componentes(Dialog, Canvas, BGroup, TGroup, SGroup).
+
+acao_procurar_pistas_ethan :-
+    free(Parent), % Destrói a janela anterior
+    ScreenText = 'Procurar Pistas',
+    ImagePath = './images/generic.jpg',
+
+    % Configuração dos componentes de texto
+    Title = ['Em cima da mesa você encontra uma passagem de barco utilizada que levava para outra ilha no mesmo dia do roubo...'],
+    Body = [
+    'O que você faz agora?',
+    '1. Continuar procurando pistas.',
+    '2. Examinar o ambiente.'
+    ],
+
+    init_fase(
+      ScreenText, ImagePath, Title, Body, Dialog, Canvas, BG, BGroup, GGroup, TGroup, SGroup
+    ),
+
+    assert(encontrou_pista(cartao_equipe_rocket)),
+    assert(encontrou_pista(passagem_barco_ethan)),
+
+    % Configuração dos botões
+    send(BGroup, append, button("Procurar Pistas", message(@prolog, acao_procurar_pistas_ethan, Dialog))),
+    send(BGroup, append, button("Examinar Quarto", message(@prolog, acao_examinar_quarto_ethan, Dialog))),
+    send(BGroup, append, button("Prosseguir", message(@prolog, fase10, 10, Dialog))),
+
+    % Configuração do botão de lista de suspeitos
+    send(SGroup, append, button("lista de suspeitos", message(@prolog, lista_suspeitos, 2, Dialog, bitmap('images/lista_suspeitos.jpg')))),
+
+    % Posicionamento dos componentes
+    componentes(Dialog, Canvas, BGroup, TGroup, SGroup).
+
+acao_examinar_quarto_ethan :-
+    free(Parent), % Destrói a janela anterior
+    ScreenText = 'Procurar Pistas',
+    ImagePath = './images/generic.jpg',
+
+    % Configuração dos componentes de texto
+    Title = ['Você nota que o quarto está bem organizado, o que parece atípico para um membro da Equipe Rocket...'],
+    Body = [
+    'O que você faz agora?',
+    '1. Procurar pistas.',
+    '2. Voltar para a caverna escura.'
+    ],
+
+    init_fase(
+      ScreenText, ImagePath, Title, Body, Dialog, Canvas, BG, BGroup, GGroup, TGroup, SGroup
+    ),
+
+    assert(encontrou_pista(cartao_equipe_rocket)),
+    assert(encontrou_pista(passagem_barco_ethan)),
+
+    % Configuração dos botões
+    send(BGroup, append, button("Procurar Pistas", message(@prolog, acao_procurar_pistas_ethan, Dialog))),
+    send(BGroup, append, button("Prosseguir", message(@prolog, fase8, 8, Dialog))),
+
+    % Configuração do botão de lista de suspeitos
+    send(SGroup, append, button("lista de suspeitos", message(@prolog, lista_suspeitos, 2, Dialog, bitmap('images/lista_suspeitos.jpg')))),
+
+    % Posicionamento dos componentes
+    componentes(Dialog, Canvas, BGroup, TGroup, SGroup).
+
---
+++ src/fase13.pl
@@ -0,0 +1,80 @@
+:- module(fase13, [fase13/2]).
+:- use_module('fase9').
+
+fase13(13, Parent) :-
+    free(Parent), % Destrói a janela anterior
+    ScreenText = 'Fase 12',
+    ImagePath = './images/generic.jpg',
+
+    % Configuração dos componentes de texto
+    Title = ['Você seguiu pelo túnel e chegou em uma caverna. Estranhamente, há sinais de algum pokémon que não anda possui pernas ou patas...'],
+    Body = [
+    'O que você faz?',
+    '1. Procurar pistas.',
+    '2. Examinar o ambiente.',
+    '3. Voltar para o túnel de pedra.'
+    ],
+
+    init_fase(
+      ScreenText, ImagePath, Title, Body, Dialog, Canvas, BG, BGroup, GGroup, TGroup, SGroup
+    ),
+
+    % Configuração dos botões
+    send(BGroup, append, button("Procurar Pistas", message(@prolog, acao_procurar_pistas_ditto, Dialog))),
+    send(BGroup, append, button("Examinar Caverna", message(@prolog, acao_examinar_caverna_ditto, Dialog))),
+    send(BGroup, append, button("Examinar Caverna", message(@prolog, fase9, 9, Dialog))),
+
+    % Configuração do botão de lista de suspeitos
+    send(SGroup, append, button("lista de suspeitos", message(@prolog, lista_suspeitos, 2, Dialog, bitmap('images/lista_suspeitos.jpg')))),
+
+    % Posicionamento dos componentes
+    componentes(Dialog, Canvas, BGroup, TGroup, SGroup).
+
+acao_procurar_pistas_ditto :-
+    Title = ['Você encontrou uma Pokebola vazia com as iniciais "SC". Isso pode indicar que Sophia Chen esteve aqui.'],
+    Body = [
+    'O que você faz agora?',
+    '1. Continuar procurando pistas.',
+    '2. Examinar o ambiente.',
+    '3. Voltar para o túnel de pedra.'
+    ],
+
+    assert(encontrou_pista(pokebola_sc)),
+    init_fase(
+      ScreenText, ImagePath, Title, Body, Dialog, Canvas, BG, BGroup, GGroup, TGroup, SGroup
+    ),
+
+    % Configuração dos botões
+    send(BGroup, append, button("Procurar Pistas", message(@prolog, acao_procurar_pistas_ditto, Dialog))),
+    send(BGroup, append, button("Examinar ambiente", message(@prolog, acao_examinar_caverna_ditto, Dialog))),
+    send(BGroup, append, button("Voltar pelo túnel", message(@prolog, fase9, 9, Dialog))),
+
+    % Configuração do botão de lista de suspeitos
+    send(SGroup, append, button("lista de suspeitos", message(@prolog, lista_suspeitos, 2, Dialog, bitmap('images/lista_suspeitos.jpg')))),
+
+    % Posicionamento dos componentes
+    componentes(Dialog, Canvas, BGroup, TGroup, SGroup).
+
+acao_examinar_caverna_ditto :-
+    Title = ['Você observa o ambiente. A caverna parece ser um esconderijo perfeito para um Ditto.'],
+    Body = [
+    'O que você faz agora?',
+    '1. Procurar pistas.',
+    '1. Continuar procurando pistas.',
+    '2. Voltar para o túnel de pedra.',
+    ],
+
+    assert(encontrou_pista(pokebola_sc)),
+    init_fase(
+      ScreenText, ImagePath, Title, Body, Dialog, Canvas, BG, BGroup, GGroup, TGroup, SGroup
+    ),
+
+    % Configuração dos botões
+    send(BGroup, append, button("Procurar Pistas", message(@prolog, acao_procurar_pistas_ditto, Dialog))),
+    send(BGroup, append, button("Voltar para o túnel", message(@prolog, fase9, 9, Dialog))),
+
+    % Configuração do botão de lista de suspeitos
+    send(SGroup, append, button("lista de suspeitos", message(@prolog, lista_suspeitos, 2, Dialog, bitmap('images/lista_suspeitos.jpg')))),
+
+    % Posicionamento dos componentes
+    componentes(Dialog, Canvas, BGroup, TGroup, SGroup).
